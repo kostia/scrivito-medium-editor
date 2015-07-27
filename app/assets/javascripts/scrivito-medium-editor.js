8ba592d8b967d3c5cdd28f5ee87e08de678d8167
Kostiantyn Kahanskyi <kostiantyn.kahanskyi@googlemail.com>
2015-07-27 16:12:24 +0200

    Added support for global options

--- app/assets/javascripts/scrivito-medium-editor.js
+++ app/assets/javascripts/scrivito-medium-editor.js
@@ -3,12 +3,15 @@
 
 ;(function() {
 
+scrivito.editors.html_editor.medium = {options: {}};
+
 scrivito.on('content', function(content) {
   if (!scrivito.in_editable_view()) { return; }
 
   $(content).find('[data-editor=medium]').each(function() {
     var dom_element = $(this);
-    new MediumEditor(dom_element, dom_element.data('medium-editor'));
+    var options = dom_element.data('medium-editor') || scrivito.editors.html_editor.medium.options;
+    new MediumEditor(dom_element, options);
     dom_element.on('input', function() {
       dom_element.scrivito('save', dom_element.html());
     });
